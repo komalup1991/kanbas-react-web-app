99b655fdc89693a84e53f8348a9d04fc6a96c871
Komal12 <upadhyay.ko@northeastern.edu>
2024-02-18 14:51:13 -0800

    implement kanbas nav and dashboard with react

---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { courses } from "../Database";
+import './index.css'
+type Course = {
+    _id: string;
+    name: string;
+    number: string;
+    startDate: string;
+    endDate: string;
+    image: string; 
+  };
+const Dashboard = () => {
+  return (
+    <div className="container-fluid wd-fixed-top-margin">
+    <h1>Dashboard</h1>
+    <hr />
+    <div className="wd-left-margin">
+      <h2>Published Courses ({courses.length})</h2>
+      <hr />
+      <div className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4 wd-dashboard-grid">
+        {courses.map((course: Course) => (
+            <div key={course._id} className="col" style={{ width: 300 }}>
+
+            <div className="card">
+            <img src={`/images/${course.image}`} className="card-img-top card-back"
+                     style={{ maxHeight: 150 }}/>
+
+              <div className="card-body text-truncate">
+              <Link className="card-title" 
+              to={`/Kanbas/Courses/${course._id}`}
+                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                    {course.name} </Link>
+                  <p className="card-text text-truncate">{course.name}</p>
+                  <Link to={`/Kanbas/Courses/${course._id}`} className="btn btn-primary">
+                    Go </Link>
+
+              </div>
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Dashboard;
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import Nav from '../Nav'
+import KanbasNavigation from "./Navigation";
+import Dashboard from './Dashboard';
+import Courses from './Courses';
+import { Routes, Route, Navigate } from "react-router-dom";
+import './styles.css'
 
 const Kanbas = () => {
   return (
+    <div className="d-flex">
     <div>
-   <Nav/>
+     
+      <KanbasNavigation />
+    </div>
+    <div style={{ flexGrow: 1 }}>
+    <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="/Account" element={<h1>Account</h1>} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Courses/:courseId/*" element={<Courses />} />
+        </Routes>
 
-    <h1>Kanbas</h1>
-   </div>
+    </div>
+  </div>
+
   )
 }
 
