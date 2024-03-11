ca6a586c4b32aac69ddd98a9978d08061ab7959f
Komal12 <upadhyay.ko@northeastern.edu>
2024-03-11 08:58:18 -0700

    Create an Assignment4 Componen

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Assignment3 from './a3';
+import Assignment4 from './a4';
+import {Routes, Route, Navigate}
+  from "react-router";
 
 import Nav from '../Nav';
 
@@ -7,9 +10,16 @@ import Nav from '../Nav';
 const Labs = () => {
   return (
     <div>
- <Nav/>
+    <Nav/>
+    <Routes>
+       <Route path="/"
+        element={<Navigate to="a3"/>}/>
+       <Route path="a3"
+        element={<Assignment3/>}/>
+       <Route path="a4"
+        element={<Assignment4/>}/>
+     </Routes>
 
-        <Assignment3 />
     </div>
   );
 }
