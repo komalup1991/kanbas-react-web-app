aad7bbaf1d82d41c3d863d723497ade07319491c
Komal12 <upadhyay.ko@northeastern.edu>
2024-03-12 14:32:04 -0700

    Add Todo without redux

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HelloRedux from './HelloRedux'
 import CounterRedux from './CounterRedux'
+import AddRedux from './AddRedux'
 
 const ReduxExamples = () => {
   return (
     <div>ReduxExamples
       <HelloRedux/>
       <CounterRedux/>
+      <AddRedux/>
     </div>
 
     
--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -11,6 +11,7 @@ import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -32,6 +33,7 @@ const Assignment4 = () => {
         <ArrayStateVariable/>
         <ParentStateComponent/>
         <ReduxExamples/>
+        <TodoList/>
 
 
 
